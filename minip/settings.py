1a12b84b2e08ebeda45039a69da10edc8dc058ad
K <ar-varun.k@RINMAC229.local>
2020-04-15 17:21:08 +0530

    UI changes

--- minip/settings.py
+++ minip/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #change before deploying
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
